7246e25a62da9013847b8475ef94f2511b43a1e2
Pedro de Oliveira <falsovsky@gmail.com>
2018-08-28 20:31:01 +0000

    change url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='BSD-3-Clause',
-    url="https://github.com/falsovsky/AnavNet",
+    url="https://github.com/falsovsky/anavnet",
     packages=setuptools.find_packages(exclude='tests'),
     install_requires=[
         'beautifulsoup4',
